d0b86d43e1cb24c77e7c6659dab494d6e3ffcc37
theom <theom@DESKTOP-GOPD1SA>
2022-11-24 15:52:30 +0100

    Ajoutez des fichiers projet.

---
+++ Exercices/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Combien de notes allez-vous saisir ? = ");
+            int nbnotes = int.Parse(Console.ReadLine());
+            int min = 20, max = 0;
+            for (int k=1; k<=nbnotes; k++)
+
+            {
+                Console.WriteLine("Note n° " + k + " = ");
+                int note = int.Parse(Console.ReadLine());    
+                max = Math.Max(max, note);
+                min = Math.Min(min, note);
+            }
+            Console.WriteLine("max =" + max);
+            Console.WriteLine("min =" + min);
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
